52cc21cc8a895f499d398f6442e416b18f4ae7d2
Firuza <firuza.19.fs@gmail.com>
2024-07-15 19:48:47 +0500

    Added lesson 4

---
+++ 3rd_lesson/hm3_2/main.go
@@ -0,0 +1,1 @@
+package hm3_2
---
+++ 4th_lesson/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//num := 6
+	//if num > 9 && num < 100 {
+	//	fmt.Println("Двузначное")
+	//}
+	//
+	//n := 6
+	//if n%2 == 1 {
+	//	fmt.Println("Число нечетное")
+	//} else {
+	//	fmt.Println("Число четное")
+	//}
+	//
+	////m := 78
+	//var m int
+	//fmt.Scanln(&m)
+	//if m >= 0 && m < 10 {
+	//	fmt.Println("Число однозначное")
+	//} else if m >= 10 && m < 100 {
+	//	fmt.Println("Число двузначное")
+	//} else if m >= 100 && m < 1000 {
+	//	fmt.Println("Число трехзначное")
+	//} else {
+	//	fmt.Println("Число слишком большое")
+	//}
+	//
+	//var a, b int
+	//fmt.Println("Add A number")
+	//fmt.Scanln(&a)
+	//fmt.Println("Add B number")
+	//fmt.Scanln(&b)
+	//if a > b {
+	//	fmt.Println("A is bigger than B")
+	//} else {
+	//	fmt.Println("B is smaller than A")
+	//}
+
+	// n1<=n2<=n3
+	// n1<=n3<=n2
+	// n2<=n1<=n3
+	// n2<=n3<=n1
+	// n3<=n1<=n2
+	// n3<=n2<=n1
+
+	var x, y, z = 9, -19, 15
+	if x <= y && y <= z {
+		fmt.Println(z, y, x)
+	} else if x <= z && z <= y {
+		fmt.Println(y, z, x)
+	} else if y <= x && x <= z {
+		fmt.Println(z, x, y)
+	} else if y <= z && z <= x {
+		fmt.Println(z, y, x)
+	} else if z <= x && x <= y {
+		fmt.Println(z, x, y)
+	} else if z <= y && y <= x {
+		fmt.Println(y, z, x)
+	}
+
+	n1 := 1
+	switch n1 {
+	case 1:
+		fmt.Println("Positive")
+	case -1:
+		fmt.Println("Negative")
+	case 0:
+		fmt.Println("Zero")
+	}
+
+	//найти наибольшее и сложить
+	var x1, y1, z1 = 9, -19, 15
+	if x1 <= y1 && y1 <= z1 {
+		fmt.Println(z1 + y1)
+	} else if x1 <= z1 && z1 <= y1 {
+		fmt.Println(y1 + z1)
+	} else if y1 <= x1 && x1 <= z1 {
+		fmt.Println(z1 + x1)
+	} else if y1 <= z1 && z1 <= x1 {
+		fmt.Println(z1 + y1)
+	} else if z1 <= x1 && x1 <= y1 {
+		fmt.Println(z1 + x1)
+	} else if z1 <= y1 && y1 <= x1 {
+		fmt.Println(y1 + z1)
+	}
+
+	//узнать високосный год if28
+	year := 1996
+
+	if year%4 == 0 && year%400 == 0 {
+		if year%100 == 0 {
+			fmt.Println("")
+		}
+		fmt.Println("leap year")
+	}
+
+}
